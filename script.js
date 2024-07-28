3834895318c69b7d228de405cb90866e3198e880
Gaurav Singh Chauhan <gkumarc121@gmail.com>
2024-07-28 22:27:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded',
+      function () {
+            const faqItems = document.querySelectorAll('.faq_item');
+
+            faqItems.forEach(item => {
+                  const question = item.querySelector('.ques_container');
+                  const answer = item.querySelector('.ans_container');
+
+                  question.addEventListener('click', () => {
+                        const isVisible = answer.style.display === 'block';
+                        answer.style.display = isVisible ? 'none' : 'block';
+
+                        const icon = question.querySelector('.faq_icon');
+                        if (answer.style.display == 'block') {
+                              // icon.className.toggle('uil uil-plus');
+                              icon.innerText = "close";
+                        }
+                        else {
+                              // icon.className.toggle('uil uil-plus');
+                              icon.innerText = "add";
+                        }
+                  })
+            })
+      })
